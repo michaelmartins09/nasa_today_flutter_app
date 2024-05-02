9ec32a6a93ac0ee0109ac3c08c59436fd592b996
Michael Martins <michaelmartins096@gmail.com>
2024-05-02 20:15:06 -0300

    test(repository): Should that repository return a AstronomyPictureEntity when request is success

---
+++ test/app/data/repositories/get_astronomy_picture_of_the_day_repository_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:nasa_today/app/data/datasources/datasources.dart';
+import 'package:nasa_today/app/data/repositories/repositories.dart';
+import 'package:nasa_today/app/domain/entities/entities.dart';
+import 'package:nasa_today/app/domain/repositories/repositories.dart';
+
+import '../../../utils/utils.dart';
+import 'get_astronomy_picture_of_the_day_repository_test.mocks.dart';
+
+@GenerateMocks([IGetAstronomyPictureOfTheDayDatasource])
+void main() {
+  late IGetAstronomyPictureOfTheDayRepository repository;
+  late IGetAstronomyPictureOfTheDayDatasource datasource;
+  late AstronomyPictureEntity pictureTodayEntityFaker;
+
+  setUp(() {
+    datasource = MockIGetAstronomyPictureOfTheDayDatasource();
+    repository = GetAstronomyPictureOfTheDayRepositoryImp(datasource);
+
+    pictureTodayEntityFaker = TestMocks.astronomyPictureToday;
+  });
+
+  test(
+    'Should that repository return a AstronomyPictureEntity when request is success',
+    () async {
+      when(repository()).thenAnswer(
+        (_) async => Future.value(pictureTodayEntityFaker),
+      );
+
+      final pictureOfTheDay = await repository();
+
+      expect(pictureOfTheDay, isA<AstronomyPictureEntity>());
+    },
+  );
+}
