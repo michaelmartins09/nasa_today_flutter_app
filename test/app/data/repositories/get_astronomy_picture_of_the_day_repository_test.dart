9143a1fd078792f8f592fe386debfbde1e270d08
Michael Martins <michaelmartins096@gmail.com>
2024-05-02 20:32:11 -0300

    test(repository): add tests for verify generic errors from request

--- test/app/data/repositories/get_astronomy_picture_of_the_day_repository_test.dart
+++ test/app/data/repositories/get_astronomy_picture_of_the_day_repository_test.dart
@@ -4,6 +4,7 @@ import 'package:mockito/mockito.dart';
 import 'package:nasa_today/app/data/datasources/datasources.dart';
 import 'package:nasa_today/app/data/repositories/repositories.dart';
 import 'package:nasa_today/app/domain/entities/entities.dart';
+import 'package:nasa_today/app/domain/exceptions/exceptions.dart';
 import 'package:nasa_today/app/domain/repositories/repositories.dart';
 
 import '../../../utils/utils.dart';
@@ -22,16 +23,31 @@ void main() {
     pictureTodayEntityFaker = TestMocks.astronomyPictureToday;
   });
 
-  test(
-    'Should that repository return a AstronomyPictureEntity when request is success',
-    () async {
-      when(repository()).thenAnswer(
-        (_) async => Future.value(pictureTodayEntityFaker),
-      );
+  group("Success Tests", () {
+    test(
+      'Should that repository return a AstronomyPictureEntity when request is success',
+      () async {
+        when(repository()).thenAnswer(
+          (_) async => Future.value(pictureTodayEntityFaker),
+        );
 
-      final pictureOfTheDay = await repository();
+        final pictureOfTheDay = await repository();
 
-      expect(pictureOfTheDay, isA<AstronomyPictureEntity>());
-    },
-  );
+        expect(pictureOfTheDay, isA<AstronomyPictureEntity>());
+      },
+    );
+  });
+
+  group("Failure Tests", () {
+    test(
+      'Should returns a NTGenericException when it occurs unexpected error',
+      () async {
+        when(repository()).thenThrow(Exception());
+
+        final future = repository();
+
+        expect(future, throwsA(isA<NTGenericException>()));
+      },
+    );
+  });
 }
