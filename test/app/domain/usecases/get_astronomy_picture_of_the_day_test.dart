3b515ebebac9cf63b2a0d7df9c8739b6c0c3f727
Michael Martins <michaelmartins096@gmail.com>
2024-05-02 20:07:58 -0300

    chore(tests): move import entity mock from utils file

--- test/app/domain/usecases/get_astronomy_picture_of_the_day_test.dart
+++ test/app/domain/usecases/get_astronomy_picture_of_the_day_test.dart
@@ -1,4 +1,3 @@
-import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -6,31 +5,20 @@ import 'package:nasa_today/app/domain/entities/entities.dart';
 import 'package:nasa_today/app/domain/repositories/repositories.dart';
 import 'package:nasa_today/app/domain/usecases/usecases.dart';
 
+import '../../../utils/utils.dart';
 import 'get_astronomy_picture_of_the_day_test.mocks.dart';
 
 @GenerateMocks([IGetAstronomyPictureOfTheDayRepository])
 void main() {
   late IGetAstronomyPictureOfTheDayUseCase usecase;
   late IGetAstronomyPictureOfTheDayRepository repository;
-  late Faker faker;
   late AstronomyPictureEntity pictureTodayEntityFaker;
 
   setUp(() {
-    faker = Faker();
-
     repository = MockIGetAstronomyPictureOfTheDayRepository();
     usecase = GetAstronomyPictureOfTheDayUsecaseImp(repository);
 
-    pictureTodayEntityFaker = AstronomyPictureEntity(
-      copyright: faker.person.name(),
-      date: faker.date.dateTime(),
-      explanation: faker.lorem.sentence(),
-      hdurl: faker.image.image(),
-      mediaType: faker.image.image(),
-      serviceVersion: faker.lorem.word(),
-      title: faker.lorem.word(),
-      url: faker.image.image(),
-    );
+    pictureTodayEntityFaker = TestMocks.astronomyPictureToday;
   });
 
   test(
