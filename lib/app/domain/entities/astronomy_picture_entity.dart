154c9106ab01953d5bea440b8ec9de27c68a395f
Michael Martins <michaelmartins096@gmail.com>
2024-05-02 18:30:27 -0300

    feat(entity): add astronomy picture of the day entity

---
+++ lib/app/domain/entities/astronomy_picture_entity.dart
@@ -0,0 +1,21 @@
+class AstronomyPictureEntity {
+  final String copyright;
+  final DateTime date;
+  final String explanation;
+  final String hdurl;
+  final String mediaType;
+  final String serviceVersion;
+  final String title;
+  final String url;
+
+  AstronomyPictureEntity({
+    required this.copyright,
+    required this.date,
+    required this.explanation,
+    required this.hdurl,
+    required this.mediaType,
+    required this.serviceVersion,
+    required this.title,
+    required this.url,
+  });
+}
