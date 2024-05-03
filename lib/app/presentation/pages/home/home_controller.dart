4b9b1723ce7f25cbb72ae65e00c08ec24a1ea1d5
Michael Martins <michaelmartins096@gmail.com>
2024-05-03 06:01:26 -0300

    feat(home): add sessiona and contents for home page

--- lib/app/presentation/pages/home/home_controller.dart
+++ lib/app/presentation/pages/home/home_controller.dart
@@ -10,23 +10,55 @@ class HomeController = _HomeControllerBase with _$HomeController;
 
 abstract class _HomeControllerBase with Store {
   final IGetAstronomyPictureOfTheDayUseCase _getAstronomyPictureOfTheDayUseCase;
+  final IGetRandomAstronomyPicturesUsecase _getRandomAstronomyPicturesUseCase;
 
-  _HomeControllerBase(this._getAstronomyPictureOfTheDayUseCase);
+  _HomeControllerBase(
+    this._getAstronomyPictureOfTheDayUseCase,
+    this._getRandomAstronomyPicturesUseCase,
+  );
 
   @observable
   bool loading = false;
 
   @observable
+  bool loadingList = false;
+
+  @observable
   AstronomyPictureDto? pictureToday;
 
+  @observable
+  ObservableList<AstronomyPictureDto> picturesList =
+      <AstronomyPictureDto>[].asObservable();
+
   @action
-  Future<void> fetch() async {
+  Future<void> fetchAstronomyPictureToday() async {
     try {
       loading = true;
       pictureToday = await _getAstronomyPictureOfTheDayUseCase();
     } catch (_) {
     } finally {
       loading = false;
+    }
+  }
+
+  @action
+  Future<void> fetchRandomAstronomyPictures({
+    int randomSize = 15,
+    DateTime? date,
+  }) async {
+    try {
+      loadingList = true;
+      picturesList.clear();
+
+      final pictures = await _getRandomAstronomyPicturesUseCase(
+        randomSize: randomSize,
+        date: date,
+      );
+
+      picturesList = pictures.asObservable();
+    } catch (_) {
+    } finally {
+      loadingList = false;
     }
   }
 }
