ec800c35d954834245a9d556fae6436eb0d17f96
Michael Martins <michaelmartins096@gmail.com>
2024-05-03 06:01:04 -0300

    feat(home): add picture card to view astronomy picture today

--- lib/app/presentation/pages/home/widgets/picture_card.dart
+++ lib/app/presentation/pages/home/widgets/picture_card.dart
@@ -1,1 +1,49 @@
+import 'package:flutter/material.dart';
 
+class PictureCard extends StatelessWidget {
+  final String picture;
+  final String title;
+
+  const PictureCard({super.key, required this.picture, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black45,
+            blurRadius: 4,
+            offset: Offset(-1, 4),
+          ),
+        ],
+        image: DecorationImage(
+          image: NetworkImage(picture),
+          fit: BoxFit.cover,
+        ),
+      ),
+      margin: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            color: Colors.black.withOpacity(0.5),
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              title,
+              maxLines: 1,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/app/presentation/pages/home/widgets/picture_session_label.dart
+++ lib/app/presentation/pages/home/widgets/picture_session_label.dart
@@ -2,19 +2,37 @@ import 'package:flutter/material.dart';
 
 class PictureSessionLabel extends StatelessWidget {
   final String label;
-  const PictureSessionLabel({super.key, required this.label});
+  final bool isLoading;
+
+  const PictureSessionLabel({
+    super.key,
+    required this.label,
+    this.isLoading = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Text(
-        label,
-        textAlign: TextAlign.left,
-        style: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              label,
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          if (isLoading)
+            const SizedBox(
+              height: 20,
+              width: 20,
+              child: CircularProgressIndicator(strokeWidth: 1),
+            )
+        ],
       ),
     );
   }
